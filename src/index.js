e124b6ad37375a7e66c72d85b3ec5ad5f5983a08
IllOll <kuzmenkijam@gmail.com>
2023-01-24 19:27:25 +0200

    feat:task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    const str = n.toString()
+    const numberTurn = str.split('').reverse().join('');
+
+
+    return parseFloat(numberTurn)
+
 }
